d37118f59625385192d3b340b88f0cfa4354bd1e
Gabriel A. Romero Aguilar <158514630+Gabrielonnn@users.noreply.github.com>
2024-02-23 16:55:41 +0000

    nueva clase

--- miPrincipal/AppLadrillos.java
+++ miPrincipal/AppLadrillos.java
@@ -1,10 +1,33 @@
 package miPrincipal;
-
 public class AppLadrillos{
 
     public static void menu(){
+        System.out.println("Vercion iterativa");
+        dibujar_pared_ite(10);
+        System.out.println("Vercion recursiva");
+        dibujar_pared_rec(10);
+    }
+        public static void dibujar_pared_ite(int n){
+            for(int i=0;i<n;i++){
+                System.out.println("xxxxxxxxxxxxxx");
+    
+            }        
+
+        }
+        public static void dibujar_pared_rec(int n){
+            if(n>0)
+            dibujar_pared_rec(n-1);
+        
+            System.out.println("xxxxxxxxxxxxxx");
+            
+
+        }
+
 
 
     }
-    
-}
+
+
+
+
+
